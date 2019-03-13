61c9c8b334a53894a239b8355d6e6032286ce943
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-13 15:28:48 +0100

    getting started

---
+++ getting-started/01-what is OOP.js
@@ -0,0 +1,6 @@
+// Object Orinted Programming is a programming paradign or style centered around OBJECTS rather than FUNCTIONS
+
+// Languages that support it
+// C#, Java, Ruby, Python, JAVASCRIPT. .
+
+// Most JavaScript Frameworks and Libraries are built with OOP paradigm
