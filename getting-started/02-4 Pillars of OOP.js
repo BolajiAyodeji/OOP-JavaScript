e5ecb0dd4f1c668d6a7c480411d566f1e78beadb
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-13 16:00:13 +0100

    update pillars

--- getting-started/02-4 Pillars of OOP.js
+++ getting-started/02-4 Pillars of OOP.js
@@ -4,7 +4,7 @@
 // POLYMORPHISM
 
 // E N C A P S U L A T I O N
-// This is basically grouping related variables and functions that operate on them into Objects
+// This is basically grouping related variables and functions that operate on them into Objects.
 
 let employee = {
   baseSalary: 30000,
@@ -15,3 +15,13 @@ let employee = {
   }
 };
 console.log(employee.getWage());
+
+// A B S T R A C T I O N
+// Helps to reduce the impact of change, hides complexity and chuns a simpler interface.
+
+// I N H E R I T A N C E
+// A mechanism that helps to eliminate redundant code.
+
+// P O L Y M O R P H I S M
+// Poly means many, morphism mean forms
+// A technique that helps to get ride of long if and else statements and switch cases.
