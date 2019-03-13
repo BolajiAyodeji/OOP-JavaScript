28f1b239e25b2a11d72ec602d4621b6fd9732a0b
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-13 15:54:15 +0100

    encapsulation

--- getting-started/02-4 Pillars of OOP.js
+++ getting-started/02-4 Pillars of OOP.js
@@ -4,12 +4,14 @@
 // POLYMORPHISM
 
 // E N C A P S U L A T I O N
-// In OOP, we group related variables and functions that operate on them into Objects
+// This is basically grouping related variables and functions that operate on them into Objects
 
-let baseSalary = 30_000;
-let overTime = 10;
-let rate = 20;
-
-function getWage(baseSalary, overTime, rate) {
-  return baseSalary + (overTime * rate)
-}
+let employee = {
+  baseSalary: 30000,
+  overTime: 10,
+  rate: 20,
+  getWage: function() {
+    return this.baseSalary + (this.overTime * this.rate);
+  }
+};
+console.log(employee.getWage());
