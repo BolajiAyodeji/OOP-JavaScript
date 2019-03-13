038ae248ef9e1c165e1c15058e46bc8bb02c1281
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-13 15:48:18 +0100

    update wage

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-let baseSalary = 30_000;
+// PROCEDURAL
+let baseSalary = 30000;
 let overTime = 10;
 let rate = 20;
 
 function getWage(baseSalary, overTime, rate) {
-  return baseSalary + (overTime * rate)
+  return baseSalary + (overTime * rate);
 }
+console.log('PROCEDURAL => ' + getWage(baseSalary, overTime, rate));
+
+// OOP
+let employee = {
+  baseSalary: 30000,
+  overTime: 10,
+  rate: 20,
+  getWage: function() {
+    return this.baseSalary + (this.overTime * this.rate);
+  }
+};
+console.log('OOP => ' + employee.getWage());
