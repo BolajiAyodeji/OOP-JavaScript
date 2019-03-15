7a3d4d15063a1f4938f1f4684c09227af5804241
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-15 08:37:18 +0100

    update inheritance

--- 03-protoypes/01-inheritance.js
+++ 03-protoypes/01-inheritance.js
@@ -3,3 +3,8 @@
 
 // Classical (In JavaScript, we don't have classes, we only have Objects).
 // Prototypical
+
+// It's important to note that there are no classes in JavaScript.
+// Functions can be used to somewhat simulate classes, but in general JavaScript is a class-less language.
+// Everything is an object.
+// When it comes to inheritance, objects inherit from objects, not classes from classes as in the "class"-ical languages.
