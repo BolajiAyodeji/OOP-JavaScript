fce46852de74fcd68a2693c6cb779c62fb62d49b
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-15 16:13:15 +0100

    avoid modifying built in methods

---
+++ 03-protoypes/08-built-in-methods-tip.js
@@ -0,0 +1,12 @@
+
+Array.prototype.shuffle = function() {
+  // ..
+};
+
+const array = [];
+array.shuffle();
+
+// DON'T MODIFY OBJECTS YOU DON'T OWN
+// DON'T OVERWRITE, ADD OR REMOVE EXISTING METHODS IN THE BUILT IN OBJECTS
+// Some framework might have used that method already, in future versions of JavaScript, that method might be added to the built in methods.
+// To play safe and avoid unnessesary bugs, avoid modifying built in objects.
