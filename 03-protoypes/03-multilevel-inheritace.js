d30d129bf9ee9805ba16bac83081cded71e609c4
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-15 14:58:42 +0100

    multilevel inheritance :zap:

---
+++ 03-protoypes/03-multilevel-inheritace.js
@@ -0,0 +1,69 @@
+
+let myArray = []
+console.log(myArray); // __proto__: Array(0)
+
+// concat: ƒ concat()
+// constructor: ƒ Array()
+// copyWithin: ƒ copyWithin()
+// entries: ƒ entries()
+// every: ƒ every()
+// fill: ƒ fill()
+// filter: ƒ filter()
+// find: ƒ find()
+// findIndex: ƒ findIndex()
+// flat: ƒ flat()
+// flatMap: ƒ flatMap()
+// forEach: ƒ forEach()
+// includes: ƒ includes()
+// indexOf: ƒ indexOf()
+// join: ƒ join()
+// keys: ƒ keys()
+// lastIndexOf: ƒ lastIndexOf()
+// length: 0
+// map: ƒ map()
+// pop: ƒ pop()
+// push: ƒ push()
+// reduce: ƒ reduce()
+// reduceRight: ƒ reduceRight()
+// reverse: ƒ reverse()
+// shift: ƒ shift()
+// slice: ƒ slice()
+// some: ƒ some()
+// sort: ƒ sort()
+// splice: ƒ splice()
+// toLocaleString: ƒ toLocaleString()
+// toString: ƒ toString()
+// unshift: ƒ unshift()
+// values: ƒ values()
+// Symbol(Symbol.iterator): ƒ values()
+// Symbol(Symbol.unscopables): {copyWithin: true, entries: true, fill: true, find: true, findIndex: true, …}
+// __proto__: Object
+
+// constructor: ƒ Object()
+// hasOwnProperty: ƒ hasOwnProperty()
+// isPrototypeOf: ƒ isPrototypeOf()
+// propertyIsEnumerable: ƒ propertyIsEnumerable()
+// toLocaleString: ƒ toLocaleString()
+// toString: ƒ toString()
+// valueOf: ƒ valueOf()
+// __defineGetter__: ƒ __defineGetter__()
+// __defineSetter__: ƒ __defineSetter__()
+// __lookupGetter__: ƒ __lookupGetter__()
+// __lookupSetter__: ƒ __lookupSetter__()
+// get __proto__: ƒ __proto__()
+// set __proto__: ƒ __proto__()
+
+
+// myArray inherits from the ARRAY base which inherits from the Object root.
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log('draw');
+  };
+}
+
+const circle = new Circle(12);
+console.log(circle)
+
+// Objects created by a given constructor will have the same prototypes.
