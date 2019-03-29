df8f386c83a270460f4cb6f4c91a782572462906
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-29 20:58:22 +0100

    modules

---
+++ 06-es6-tooling/01-modules.js
@@ -0,0 +1,19 @@
+
+// In JavaScript, the word “modules” refers to small units of independent, reusable code.
+// They are the foundation of many JavaScript design patterns and are critically necessary
+// when building any substantial JavaScript-based application.
+
+// In simpler terms, modules help you to write code in your module and expose only
+// those parts of the code that should be accessed by other parts of your code.
+
+//  Read more here: https://bolajiayodeji.com/introduction-to-es6-modules/
+
+// Maintain
+// Reuse
+// Abstract
+
+
+// AMD (Browser apps)
+// Common JS (Node.js) *
+// UMD (Both)
+// ES6 Modules (All) *
--- main.js
+++ main.js
@@ -1,45 +1,16 @@
 
-const _items = new WeakMap();
+const _radius = new WeakMap();
 
-class Stack {
-  constructor() {
-    _items.set(this, []);
+class Circle {
+  constructor(radius) {
+    _radius.set(this, radius);
   }
 
-  push(obj) {
-    _items.get(this).push(obj);
-  }
-
-  pop() {
-    const items = _items.get(this);
-
-    if(items.length === 0) {
-      throw new Error('Stack is empty')
-    }
-    else {
-      return items.pop();
-    }
-  }
-
-  peek() {
-    const items = _items.get(this);
-
-    if(items.length === 0) {
-      throw new Error('Stack is empty')
-    }
-    else {
-      return items[items.length - 1];
-    }
-  }
-
-  get count() {
-  return _items.get(this).length;
+  draw() {
+    console.log('Circle with radius ' + _radius.get(this));
   }
 }
 
-const stack = new Stack();
-
-stack.count
-stack.peek();
-stack.push('a');
-stack.pop();
+const c = new Circle(11);
+console.log(_radius.get(c));
+c.draw();
