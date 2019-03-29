39357f25042af3d22eaae6210431619356dcf209
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-29 22:23:33 +0100

    modules cheat sheet

--- cheat-sheets/modules-cheat-sheet.js
+++ cheat-sheets/modules-cheat-sheet.js
@@ -3,22 +3,21 @@
 //  - AMD / Asynchronous Module Definition (Browser)
 //  - CommonJS (Node)
 //  - UMD / Universal Module Definition (Browser + Node)
-//  - ES6 Modules 
+//  - ES6 Modules
 
 // CommonJS (Used in Node)
-// Exporting 
-module.exports.Cirlce = Circle; 
-// Importing 
+// Exporting
+module.exports.Cirlce = Circle;
+// Importing
 const Circle = require('./circle');
 
 // ES6 Modules (Used in Browser)
 // Exporting
 export class Square {}
-// Importing 
-import {Square} from './square'; 
+// Importing
+import {Square} from './square';
 
-// We use Babel to transpile our modern JavaScript code 
-// into code that browsers can understand (typically ES5). 
+// We use Babel to transpile our modern JavaScript code
+// into code that browsers can understand (typically ES5).
 
-// We use Webpack to combine our JavaScript files into a
-// bundle. 
+// We use Webpack to combine our JavaScript files into a bundle.
